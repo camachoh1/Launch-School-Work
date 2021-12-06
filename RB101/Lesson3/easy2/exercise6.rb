5e88ca8fcc02810c6dd17a0dc6e3b8259e48f4c5
Harold Camacho <harold.camacho.diaz@gmail.com>
2021-12-06 13:54:10 -0500

    updating folder

--- RB101/Lesson3/easy2/exercise6.rb
+++
@@ -1,9 +0,0 @@
-# how can we add the family pet "Dino" to our usual array:
-
-flinstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
-
-flinstones << "Dino"
-
-p flinstones
-
-#my answer: I used the << method which has the same function as #push. It appends an object to an array. It is a mutable method. 
--- RB101/Lesson3/easy2/exercise9.rb
+++
@@ -1,7 +0,0 @@
-# write a one-liner to count the number of lower-case 't' characters in the following string:
-
-statement = "The Flinstones Rock!"
-
-puts statement.count('t')
-
-#This one was pretty straight forward, I just needed to use the count method which receives a value as argument and it returns the number of times the argument ( in this case lowercase 't') appears in the string. 
